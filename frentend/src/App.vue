7efbbf0c1332628547147e0b2563169967e1d110
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-03-17 00:09:55 +0100

    premieres instalations

---
+++ frentend/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+
+import { RouterLink } from 'vue-router';
+
+import Navbar from './components/Navbar.vue'
+
+import Footer from './components/Footer.vue'
+
+import Product from './components/Product.vue'
+
+import Dashboard from './components/Admin/Dashboard.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Footer,
+    Product,
+    Dashboard
+  }
+}
+
+</script>
+
+<template>
+  <!-- <navbar></navbar> -->
+  <!-- <Product/> -->
+  <Dashboard/>
+  <!-- <Footer/> -->
+  
+</template>
+
+<style>
+
+</style>
