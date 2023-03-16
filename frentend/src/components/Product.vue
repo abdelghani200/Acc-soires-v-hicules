db1391b8888b5fa432db5059c3afea381f4da70b
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-03-17 00:05:13 +0100

    ProductVue

---
+++ frentend/src/components/Product.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="jewellery_section">
+        <div id="jewellery_main_slider" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="container">
+                        <h1 class="fashion_taital">Voitures Accessories</h1>
+                        <div class="fashion_section_2">
+                            <div class="row">
+                                <div class="col-lg-4 col-sm-4">
+                                    <div class="box_main">
+                                        <h4 class="shirt_text">Jumkas</h4>
+                                        <p class="price_text">Start Price <span style="color: #262626;">$ 100</span></p>
+                                        <div class="jewellery_img"><img src="../../public/images/v_1.jpg"></div>
+                                        <div class="btn_main">
+                                            <div class="buy_bt"><a href="#">Buy Now <span style="color: aqua;"><i class="fa-solid fa-cart-shopping"></i></span></a></div>
+                                            <div class="seemore_bt"><a href="#">Sow<i class="fa-sharp fa-regular fa-eye"></i></a></div>
+                                            <div class="seemore_bt"><a href="#">See More</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-sm-4">
+                                    <div class="box_main">
+                                        <h4 class="shirt_text">Necklaces</h4>
+                                        <p class="price_text">Start Price <span style="color: #262626;">$ 100</span></p>
+                                        <div class="jewellery_img"><img src="../../public/images/v_2.jpg"></div>
+                                        <div class="btn_main">
+                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
+                                            <div class="seemore_bt"><a href="#">See More</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-sm-4">
+                                    <div class="box_main">
+                                        <h4 class="shirt_text">Kangans</h4>
+                                        <p class="price_text">Start Price <span style="color: #262626;">$ 100</span></p>
+                                        <div class="jewellery_img"><img src="../../public/images/v_3.jpg"></div>
+                                        <div class="btn_main">
+                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
+                                            <div class="seemore_bt"><a href="#">See More</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="container">
+                        <h1 class="fashion_taital">Motos Accessories</h1>
+                        <div class="fashion_section_2">
+                            <div class="row">
+                                <div class="col-lg-4 col-sm-4">
+                                    <div class="box_main">
+                                        <h4 class="shirt_text">Jumkas</h4>
+                                        <p class="price_text">Start Price <span style="color: #262626;">$ 100</span></p>
+                                        <div class="jewellery_img"><img src="../../public/images/v_1.jpg"></div>
+                                        <div class="btn_main">
+                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
+                                            <div class="seemore_bt"><a href="#">See More</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-sm-4">
+                                    <div class="box_main">
+                                        <h4 class="shirt_text">Necklaces</h4>
+                                        <p class="price_text">Start Price <span style="color: #262626;">$ 100</span></p>
+                                        <div class="jewellery_img"><img src="../../public/images/v_2.jpg"></div>
+                                        <div class="btn_main">
+                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
+                                            <div class="seemore_bt"><a href="#">See More</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-sm-4">
+                                    <div class="box_main">
+                                        <h4 class="shirt_text">Kangans</h4>
+                                        <p class="price_text">Start Price <span style="color: #262626;">$ 100</span></p>
+                                        <div class="jewellery_img"><img src="../../public/images/v_1.jpg"></div>
+                                        <div class="btn_main">
+                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
+                                            <div class="seemore_bt"><a href="#">See More</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="container">
+                        <h1 class="fashion_taital">Voitures Accessories</h1>
+                        <div class="fashion_section_2">
+                            <div class="row">
+                                <div class="col-lg-4 col-sm-4">
+                                    <div class="box_main">
+                                        <h4 class="shirt_text">Jumkas</h4>
+                                        <p class="price_text">Start Price <span style="color: #262626;">$ 100</span></p>
+                                        <div class="jewellery_img"><img src="../../public/images/v_3.jpg"></div>
+                                        <div class="btn_main">
+                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
+                                            <div class="seemore_bt"><a href="#">See More</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-sm-4">
+                                    <div class="box_main">
+                                        <h4 class="shirt_text">Necklaces</h4>
+                                        <p class="price_text">Start Price <span style="color: #262626;">$ 100</span></p>
+                                        <div class="jewellery_img"><img src="../../public/images/v_1.jpg"></div>
+                                        <div class="btn_main">
+                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
+                                            <div class="seemore_bt"><a href="#">See More</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-sm-4">
+                                    <div class="box_main">
+                                        <h4 class="shirt_text">Kangans</h4>
+                                        <p class="price_text">Start Price <span style="color: #262626;">$ 100</span></p>
+                                        <div class="jewellery_img"><img src="../../public/images/v_2.jpg"></div>
+                                        <div class="btn_main">
+                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
+                                            <div class="seemore_bt"><a href="#">See More</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="" role="button" data-slide="prev">
+                <i class="fa fa-angle-left"></i>
+            </a>
+            <a class="carousel-control-next" href="" role="button" data-slide="next">
+                <i class="fa fa-angle-right"></i>
+            </a>
+            <div class="loader_main">
+                <div class="loader"></div>
+            </div>
+        </div>
+    </div>
+</template>
