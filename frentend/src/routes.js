9cce4da650c7d5b1a115aa9d617da0ebd51c4b8c
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-04-06 14:22:01 +0000

    update routes.js

--- frentend/src/routes.js
+++ frentend/src/routes.js
@@ -4,9 +4,28 @@ import Login from './components/Login.vue';
 
 import Register from './components/Register.vue';
 
-import Dashboard  from './components/Admin/Dashboard.vue';
+import Dashboard  from './views/Dashboard.vue';
 
 import Cart from './components/Cart.vue';
+
+import AddCategorie from './components/Admin/categories/AddCategorie.vue';
+
+import order from './components/Admin/orders/order.vue';
+
+import produits from './components/Admin/produits/produits.vue';
+
+import Product from './components/Product.vue';
+
+import DetailProduct from './components/DetailProduct.vue';
+
+import DisplayCategories from './components/Admin/categories/DisplayCategories.vue';
+
+import Statiques from './components/Admin/Statiques.vue';
+
+import ProduitsVue from './components/ProduitsVue.vue';
+
+
+
 
 const routes = [
     {
@@ -24,6 +43,38 @@ const routes = [
     {
         path: '/Cart',
         component: Cart,
+    },
+    {
+        path: '/AddCategorie',
+        component: AddCategorie,
+    },
+    {
+        path: '/order',
+        component: order,
+    },
+    {
+        path:'/produits',
+        component: produits,
+    },
+    {
+        path: '/Product',
+        component: Product,
+    },
+    {
+        path: '/details',
+        component: DetailProduct
+    },
+    {
+        path: '/DisplayCategories',
+        component: DisplayCategories
+    },
+    {
+        path: '/statiques',
+        component: Statiques
+    },
+    {
+        path: '/ProduitsVue',
+        component: ProduitsVue
     }
 
 ]
