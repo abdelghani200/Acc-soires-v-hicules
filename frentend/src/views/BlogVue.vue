2486b4c09c4a16d337b1ef891defc3520f083938
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-04-14 15:49:19 +0000

    page bloge de site

---
+++ frentend/src/views/BlogVue.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container" style="margin-top: 190px;">
+        <h1>À propos de notre site e-commerce</h1>
+        <hr>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="/images/1234.jpg" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h2>Nous sommes passionnés d'automobiles</h2>
+                <p>Nous sommes une entreprise spécialisée dans la vente d'accessoires pour véhicules en ligne. Notre équipe
+                    est constituée de passionnés d'automobiles et nous mettons tout en oeuvre pour offrir à nos clients un
+                    service de qualité, des produits de qualité et une expérience d'achat en ligne agréable.</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Nous proposons une large gamme de produits</h2>
+                <p>Nous proposons une large gamme de produits pour répondre aux besoins de nos clients, que ce soit pour
+                    leur voiture, moto ou tout autre type de véhicule. Nous avons des produits pour la sécurité,
+                    l'entretien, la décoration et plus encore.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="/images/4321.jpg" class="img-fluid">
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="/images/0987.jpg" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h2>Nous offrons un service de qualité</h2>
+                <p>Nous sommes à votre disposition pour vous aider à trouver les accessoires dont vous avez besoin pour
+                    votre véhicule. Si vous avez des questions ou des commentaires sur notre site e-commerce, n'hésitez pas
+                    à nous contacter. Nous offrons un service de qualité pour vous aider à trouver les meilleurs produits
+                    pour votre véhicule.</p>
+            </div>
+        </div>
+        <hr>
+    <p><strong>Adresse :</strong> 123 rue des Accessoires, 75000 Paris</p>
+    <p><strong>Téléphone :</strong> 01 23 45 67 89</p>
+    <p><strong>Email :</strong> contact@monsiteecommerce.com</p>
+</div></template>
