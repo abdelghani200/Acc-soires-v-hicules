8b6484a39ebd64017d5a49f1b5e773ecd22382e1
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-04-14 15:50:16 +0000

    update categorie * @param categorie

--- frentend/src/components/Admin/categories/UpdateCategorie.vue
+++ frentend/src/components/Admin/categories/UpdateCategorie.vue
@@ -1,54 +1,79 @@
 <template>
-    <form class="user" @submit.prevent="updateCategory">
-        <div class="form-group">
-            <div class="col-sm-6 mb-3 mb-sm-0">
-                <input type="text" class="form-control form-control-user" v-model="name" placeholder="Name">
+    <div class="col-lg-12 grid-margin stretch-card">
+        <h4 class="card-title"><span></span>Update_Products</h4>
+        <form class="user" @submit.prevent="updateCategory">
+            <div class="form-group">
+                <div class="col-sm-6 mb-3 mb-sm-0">
+                    <input type="text" class="form-control form-control-user" v-model="category.name">
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-6 mb-3 mb-sm-0">
-                <input type="text" class="form-control form-control-user" v-model="description" placeholder="Description">
+            <div class="form-group">
+                <div class="col-sm-6 mb-3 mb-sm-0">
+                    <input type="text" class="form-control form-control-user" v-model="category.description">
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <input type="file" class="form-control form-control-user" ref="image" placeholder="Image">
-        </div>
-        <div class="form-group">
-            <div class="col-sm-6 mb-3 mb-sm-0">
-                <input type="submit" class="btn btn-primary btn-user" value="Mettre à jour">
+            <div class="form-group">
+                <input type="file" class="form-control form-control-user" id="image" @change="onImageChange">
             </div>
-        </div>
-    </form>
+            <div class="form-group">
+                <div class="col-sm-6 mb-3 mb-sm-0">
+                    <button type="submit" class="btn btn-primary btn-user">Mettre à jour</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
+
 <script>
+import axios from 'axios'
+
+
 export default {
-    props: ['categoryId', 'categoryName', 'categoryDescription'],
 
     data() {
         return {
-            name: this.categoryName,
-            description: this.categoryDescription,
+            category: { name: '', description: '' },
             image: null
         }
     },
 
-    methods: {
-        updateCategory() {
-            // récupérer les données de formulaire
-            let formData = new FormData()
-            formData.append('name', this.name)
-            formData.append('description', this.description)
-            formData.append('image', this.$refs.image.files[0])
+    props: {
+        id: {
+            type: Number,
+            required: true
+        }
+    },
+    mounted() {
+        this.getCategories()
+    },
 
-            // effectuer une requête POST pour mettre à jour la catégorie
-            axios.post(`/categories/${this.categoryId}?_method=PUT`, formData)
+    methods: {
+
+        getCategories() {
+
+            axios.get(`api/categories/${this.id}`)
                 .then(response => {
-                    // rediriger l'utilisateur vers la page des catégories
-                    this.$router.push({ name: 'categories' })
-                })
-                .catch(error => {
+                    this.category = response.data
+                    console.log(this.category);
+                }).catch(error => {
                     console.log(error)
                 })
+
+        },
+        onImageChange(event) {
+            this.image = event.target.files[0];
+        },
+
+        updateCategory() {
+            const categoryId = this.$route.params.id;
+            axios.put(`api/categories/${categoryId}`, this.category)
+                .then(response => {
+                    console.log(response.data);
+                    this.$router.push('/DisplayCategories')
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
     }
 }
