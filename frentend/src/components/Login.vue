b65a37cc03c80ed9105b250ea0af7281ea88f74a
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-04-14 15:46:11 +0000

    update page login

--- frentend/src/components/Login.vue
+++ frentend/src/components/Login.vue
@@ -9,36 +9,32 @@ export default {
         return {
             email: '',
             password: '',
-            showLoginModal: false,
-            showModal: false,
+            errorMessage: '',
+            isAuthenticated: false
         }
     },
     methods: {
 
         async loginAdmin() {
             try {
-                const response = await axios.post('/api/login', {
+                const response = await axios.post('api/login', {
                     email: this.email,
                     password: this.password
                 });
 
                 console.log(response.data);
-                this.isLoggedIn = true
-                // if (response.data.status === 200) {
-                //     alert('success');
-                // } else {
-                //     alert('error');
-                // }
-
-                this.$router.push('/Dashboard');
+                const { token, redirect_url } = response.data;
+                const userId = response.data.data.user_id;
+                console.log(userId);
+                localStorage.setItem("isLoggedIn", true);
+                localStorage.setItem('access_token', token);
+                localStorage.setItem('user_id', userId);
+                window.location.href = redirect_url;
 
 
             } catch (error) {
                 alert('error');
             }
-        },
-        hideLoginModal() {
-            this.showModal = false
         },
     }
 };
@@ -48,104 +44,63 @@ export default {
     
 <template>
     <!-- Outer Row -->
-    <div class="row justify-content-center">
 
-        <div class="col-xl-10 col-lg-12 col-md-9">
+    <div style="margin-top: 130px;">
+        <div class="row justify-content-center">
 
-            <div class="card o-hidden border-0 shadow-lg my-5">
-                <div class="card-body p-0">
-                    <!-- Nested Row within Card Body -->
-                    <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-login-image"><img src="../../public/1.png">
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="p-5">
-                                <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                </div>
-                                <form class="user" @submit.prevent="loginAdmin">
-                                    <div class="form-group">
-                                        <input type="email" class="form-control form-control-user" v-model="email"
-                                            id="exampleInputEmail" aria-describedby="emailHelp"
-                                            placeholder="Enter Email Address...">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"><img src="../../public/1.png">
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
-                                    <div class="form-group">
-                                        <input type="password" class="form-control form-control-user" v-model="password"
-                                            id="exampleInputPassword" placeholder="Password">
+                                    <div v-if="errorMessage && errorMessage.trim()" class="alert alert-danger" role="alert">
+                                        {{ errorMessage }}
                                     </div>
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox small">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck">
-                                            <label class="custom-control-label" for="customCheck">Remember
-                                                Me</label>
+                                    <form class="user" @submit.prevent="loginAdmin">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user" v-model="email"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Enter Email Address...">
                                         </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user" v-model="password"
+                                                id="exampleInputPassword" placeholder="Password">
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block" v-on="refreshPage">
+                                            Login
+                                        </button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="#">Forgot Password?</a>
                                     </div>
-                                    <button type="submit" class="btn btn-primary btn-user btn-block">
-                                        Login
-                                    </button>
-                                    <!-- <hr>
-                                    <a href="" class="btn btn-google btn-user btn-block">
-                                        <i class="fab fa-google fa-fw"></i> Login with Google
-                                    </a>
-                                    <a href="" class="btn btn-facebook btn-user btn-block">
-                                        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                    </a> -->
-                                </form>
-                                <hr>
-                                <div class="text-center">
-                                    <a class="small" href="#">Forgot Password?</a>
-                                </div>
-                                <div class="text-center">
-                                    <RouterLink class="small" to="/Register">Create an Account!</RouterLink>
+                                    <div class="text-center">
+                                        <RouterLink class="small" to="/Register">Create an Account!</RouterLink>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
             </div>
 
         </div>
-
     </div>
 </template>
 
-
-
-<style>
-.modal {
-  display: block;
-  position: fixed;
-  margin-top: 6%;
-  z-index: 1;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 95%;
-  background-color: rgba(0,0,0,0.4);
-}
-
-/* margin: 15% auto; */
-.modal-content {
-  background-color: #fefefe;
-  margin-top: 4%;
-  padding: 30px;
-  border: 1px solid #888;
-  width: 80%;
-  height: 90%;
-  margin-left: 10%;
-}
-
-.close {
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: black;
-  text-decoration: none;
-  cursor: pointer;
-}
-</style>
