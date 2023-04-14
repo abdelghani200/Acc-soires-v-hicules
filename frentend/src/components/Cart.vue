2f6bd14f3e2c27d461834d08e827b00f4aa7ab36
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-04-14 15:45:18 +0000

    afficher product in cart

--- frentend/src/components/Cart.vue
+++ frentend/src/components/Cart.vue
@@ -1,7 +1,5 @@
-
-
 <template>
-  <section class="h-100 h-custom" style="background-color: #eee;">
+  <section class="h-100 h-custom" style="background-color: #eee;margin-top: 160px;">
     <div class="container py-5 h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col">
@@ -18,7 +16,7 @@
                   <div class="d-flex justify-content-between align-items-center mb-4">
                     <div>
                       <p class="mb-1">Shopping cart</p>
-                      <p class="mb-0">You have 4 items in your cart</p>
+                      <p class="mb-0">You have {{ number_product }} items in your cart</p>
                     </div>
                     <div>
                       <p class="mb-0"><span class="text-muted">Sort by:</span> <a href="#!" class="text-body">price <i
@@ -26,52 +24,23 @@
                     </div>
                   </div>
 
-                  <div class="card mb-3">
+                  <div v-for="(item, index) in cartItems" :key="index" class="card mb-3">
                     <div class="card-body">
                       <div class="d-flex justify-content-between">
                         <div class="d-flex flex-row align-items-center">
                           <div>
-                            <img
-                              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                              class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
+                            <img :src="item.image" class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
                           </div>
                           <div class="ms-3">
-                            <h5>Iphone 11 pro</h5>
-                            <p class="small mb-0">256GB, Navy Blue</p>
+                            <h5>{{ item.name }}</h5>
                           </div>
                         </div>
+                        <div style="width: 50px;">
+                          <input type="number" min="1" max="10" v-model="item.quantity" @change="updateQuantity(item)">
+                        </div>
                         <div class="d-flex flex-row align-items-center">
-                          <div style="width: 50px;">
-                            <h5 class="fw-normal mb-0">2</h5>
-                          </div>
                           <div style="width: 80px;">
-                            <h5 class="mb-0">$900</h5>
-                          </div>
-                          <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt"></i></a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="card mb-3">
-                    <div class="card-body">
-                      <div class="d-flex justify-content-between">
-                        <div class="d-flex flex-row align-items-center">
-                          <div>
-                            <img
-                              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img2.webp"
-                              class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
-                          </div>
-                          <div class="ms-3">
-                            <h5>Samsung galaxy Note 10 </h5>
-                            <p class="small mb-0">256GB, Navy Blue</p>
-                          </div>
-                        </div>
-                        <div class="d-flex flex-row align-items-center">
-                          <div style="width: 50px;">
-                            <h5 class="fw-normal mb-0">2</h5>
-                          </div>
-                          <div style="width: 80px;">
-                            <h5 class="mb-0">$900</h5>
+                            <h5 class="mb-0">{{ item.price }}</h5>
                           </div>
                           <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt"></i></a>
                         </div>
@@ -79,62 +48,11 @@
                     </div>
                   </div>
 
-                  <div class="card mb-3">
-                    <div class="card-body">
-                      <div class="d-flex justify-content-between">
-                        <div class="d-flex flex-row align-items-center">
-                          <div>
-                            <img
-                              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img3.webp"
-                              class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
-                          </div>
-                          <div class="ms-3">
-                            <h5>Canon EOS M50</h5>
-                            <p class="small mb-0">Onyx Black</p>
-                          </div>
-                        </div>
-                        <div class="d-flex flex-row align-items-center">
-                          <div style="width: 50px;">
-                            <h5 class="fw-normal mb-0">1</h5>
-                          </div>
-                          <div style="width: 80px;">
-                            <h5 class="mb-0">$1199</h5>
-                          </div>
-                          <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt"></i></a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
 
-                  <div class="card mb-3 mb-lg-0">
-                    <div class="card-body">
-                      <div class="d-flex justify-content-between">
-                        <div class="d-flex flex-row align-items-center">
-                          <div>
-                            <img
-                              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img4.webp"
-                              class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
-                          </div>
-                          <div class="ms-3">
-                            <h5>MacBook Pro</h5>
-                            <p class="small mb-0">1TB, Graphite</p>
-                          </div>
-                        </div>
-                        <div class="d-flex flex-row align-items-center">
-                          <div style="width: 50px;">
-                            <h5 class="fw-normal mb-0">1</h5>
-                          </div>
-                          <div style="width: 80px;">
-                            <h5 class="mb-0">$1799</h5>
-                          </div>
-                          <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt"></i></a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+
 
                 </div>
-                <div class="col-lg-5">
+                <!-- <div class="col-lg-5">
 
                   <div class="card bg-primary text-white rounded-3">
                     <div class="card-body">
@@ -215,7 +133,7 @@
                     </div>
                   </div>
 
-                </div>
+                </div> -->
 
               </div>
 
@@ -229,3 +147,45 @@
 
 
 
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: "Cart",
+
+
+  data() {
+    return {
+      cartItems: []
+
+    }
+  },
+  methods: {
+    async getCart() {
+      try {
+        const response = await axios.get('api/cart', {
+          headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+          }
+        })
+        this.cartItems = response.data.data
+        // console.log(1);
+        console.log(this.cartItems);
+        console.log(response);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+
+  mounted() {
+    this.getCart()
+  }
+
+
+
+}
+
+
+</script>
