8057ca71322403ff4e9c8c61641282b9a0d5bf6f
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-04-14 15:44:58 +0000

    add links

--- frentend/src/views/Dashboard.vue
+++ frentend/src/views/Dashboard.vue
@@ -19,10 +19,14 @@
                         <li class="nav-item">
                             <RouterLink class="nav-link" to="/produits"><i class="fas fa-box" style="color: gray;"></i>
                                 Products</RouterLink>
+                            <RouterLink class="nav-link" to="/add"><i class="fas fa-box" style="color: gray;"></i>
+                                AddProduct</RouterLink>
                         </li>
                         <li class="nav-item">
                             <RouterLink class="nav-link" to="/DisplayCategories"><i class="fas fa-tags"
                                     style="color: gray;"></i> Categories</RouterLink>
+                            <RouterLink class="nav-link" to="/addCategorie"><i class="fas fa-tags"
+                                    style="color: gray;"></i> AddCategorie</RouterLink>
                         </li>
                         <li class="nav-item">
                             <RouterLink class="nav-link" to="/clients"><i class="fas fa-users" style="color: gray;"></i>
@@ -64,47 +68,6 @@
                         </ul>
                     </nav>
                 </div>
-                <!-- <div class="row">
-                    <div class="col-md-4 stretch-card grid-margin">
-                        <div class="card card-img-holder text-white"
-                            style=" background-color: #dc3545;background-image: linear-gradient(to bottom, #dc3545 150%, #bd2130 100%);">
-                            <div class="card-body">
-                                <i class="fas fa-chart-bar" style="color: black;"></i>
-                                <h4 class="font-weight-normal mb-3">Weekly Sales <i
-                                        class="mdi mdi-chart-line mdi-24px float-right"></i>
-                                </h4>
-                                <h2 class="mb-5">$ 15,0000</h2>
-                                <h6 class="card-text">Increased by 60%</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 stretch-card grid-margin">
-                        <div class="card card-img-holder text-white"
-                            style="background-color: #17a2b8;background-image: linear-gradient(to bottom, #17a2b8 150%, #117a8b 100%);">
-                            <div class="card-body">
-                                <i class="fas fa-chart-bar" style="color: black;"></i>
-                                <h4 class="font-weight-normal mb-3">Weekly Orders <i
-                                        class="mdi mdi-bookmark-outline mdi-24px float-right"></i>
-                                </h4>
-                                <h2 class="mb-5">45,6334</h2>
-                                <h6 class="card-text">Decreased by 10%</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 stretch-card grid-margin">
-                        <div class="card card-img-holder text-white"
-                            style="background-color: #28a745;background-image: linear-gradient(to bottom, #28a745 150%, #1e7e34 80%);">
-                            <div class="card-body">
-                                <i class="fas fa-chart-bar" style="color: black;"></i>
-                                <h4 class="font-weight-normal mb-3">Visitors Online <i
-                                        class="mdi mdi-diamond mdi-24px float-right"></i>
-                                </h4>
-                                <h2 class="mb-5">95,5741</h2>
-                                <h6 class="card-text">Increased by 5%</h6>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
                 <RouterView />
             </main>
         </div>
